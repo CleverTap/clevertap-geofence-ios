5dbda370ea5f91cca185215d2bb4432ec2cd0198
Yogesh Singh <yogesh@clevertap.com>
2020-07-10 21:23:09 +0530

    SDK-283: removed redundant tests manifest file

--- Tests/CleverTapGeofenceTests/CleverTapGeofenceTests.swift
+++ Tests/CleverTapGeofenceTests/CleverTapGeofenceTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import CleverTapGeofence
 
 final class CleverTapGeofenceTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
