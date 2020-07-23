37586672929ff290fea443e00e0969c86153cbf0
Yogesh <yogesh.singh.2810@gmail.com>
2020-07-24 03:08:13 +0530

    SDK-104: added capability to configure logger for clients

--- Sources/CleverTapGeofenceUtils.swift
+++ Sources/CleverTapGeofenceUtils.swift
@@ -13,7 +13,24 @@ internal struct CleverTapGeofenceUtils {
     
     internal static let pluginVersion = "100000"
     
+    
     internal static let geofencesNotification = NSNotification.Name("CleverTapGeofencesDidUpdateNotification")
     
-    internal static let logger = OSLog(subsystem: "com.clevertap.CleverTapGeofence", category: "CleverTapGeofence")
+    
+    private static let logger = OSLog(subsystem: "com.clevertap.CleverTapGeofence", category: "CleverTapGeofence")
+    
+    
+    internal static func log(_ message: StaticString, type: CleverTapGeofenceLogLevel = CleverTapGeofenceLogLevel.Error) {
+        
+        switch CleverTapGeofence.monitor.logLevel {
+        case .Error:
+            os_log(message, log: logger, type: .error)
+        case .Debug:
+            os_log(message, log: logger, type: .debug)
+        case .Off:
+            break
+        default:
+            break
+        }
+    }
 }
