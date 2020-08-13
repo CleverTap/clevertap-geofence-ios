b07ba4f7dc93e638c8652ba2e5ed5d1ed884af4f
Aditi3 <aditi.gvc@gmail.com>
2020-08-13 15:45:52 +0530

    SDK-104: add Carthage Example

---
+++ Examples/CarthageExample/GeofenceCarthageExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeofenceCarthageExample
+//
+//  Created by Yogesh Singh on 09/07/20.
+//  Copyright © 2020 CleverTap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
