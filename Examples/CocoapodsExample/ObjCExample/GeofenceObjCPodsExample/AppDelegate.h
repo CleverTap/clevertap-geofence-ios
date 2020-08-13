9ce188767fb344839116c41bc310594c223c7e64
Aditi3 <aditi.gvc@gmail.com>
2020-08-13 16:29:07 +0530

    SDK-104: update ObjC Example

--- Examples/CocoapodsExample/ObjCExample/GeofenceObjCPodsExample/AppDelegate.h
+++ Examples/CocoapodsExample/ObjCExample/GeofenceObjCPodsExample/AppDelegate.h
@@ -1,15 +1,8 @@
-//
-//  AppDelegate.h
-//  GeofenceObjCPodsExample
-//
-//  Created by Yogesh Singh on 09/07/20.
-//  Copyright © 2020 CleverTap. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+@property (strong, nonatomic) UIWindow *window;
 
 @end
 
--- Examples/CocoapodsExample/ObjCExample/GeofenceObjCPodsExample/AppDelegate.m
+++ Examples/CocoapodsExample/ObjCExample/GeofenceObjCPodsExample/AppDelegate.m
@@ -18,10 +18,8 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    // other app setup logic
+    // Override point for customization after application launch.
     
     [CleverTap autoIntegrate];
     CleverTapGeofence.logLevel = CleverTapGeofenceLogLevelDebug;
@@ -29,27 +27,32 @@
     return YES;
 }
 
-- (void)someScenarioWhereLocationMonitoringShouldBeOff {
-    
-    [[CleverTapGeofence monitor] stop]; // or you can use CleverTapGeofence.monitor.stop;
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
 
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options  API_AVAILABLE(ios(13.0)){
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions  API_AVAILABLE(ios(13.0)){
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 
 @end
+
