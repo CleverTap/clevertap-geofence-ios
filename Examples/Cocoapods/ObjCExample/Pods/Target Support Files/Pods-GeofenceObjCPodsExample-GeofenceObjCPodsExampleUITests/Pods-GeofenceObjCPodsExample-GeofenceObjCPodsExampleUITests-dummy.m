46770fce8ec15c417bd0854504c363b6300210b3
Aditi3 <aditi.gvc@gmail.com>
2020-08-13 15:40:05 +0530

    SDK-104: update examples

--- Examples/Cocoapods/ObjCExample/Pods/Target Support Files/Pods-GeofenceObjCPodsExample-GeofenceObjCPodsExampleUITests/Pods-GeofenceObjCPodsExample-GeofenceObjCPodsExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GeofenceObjCPodsExample_GeofenceObjCPodsExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GeofenceObjCPodsExample_GeofenceObjCPodsExampleUITests
-@end
