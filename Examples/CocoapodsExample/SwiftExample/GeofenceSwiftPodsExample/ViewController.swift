dc793a10b73bc8a9c7a0fe62c8c00c6a8c5bd116
Yogesh Singh <yogesh@clevertap.com>
2020-08-28 22:13:02 +0530

    SDK-104: updated example apps

--- Examples/CocoapodsExample/SwiftExample/GeofenceSwiftPodsExample/ViewController.swift
+++ Examples/CocoapodsExample/SwiftExample/GeofenceSwiftPodsExample/ViewController.swift
@@ -2,11 +2,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func performActionOnGeofenceEnter() {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "CleverTapGeofenceEntered"), object: nil, queue: OperationQueue.main) { (notification) in
+            print("Perform custom action on Geofence Enter event")
+        }
+    }
+    
+    func performActionOnGeofenceExit() {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "CleverTapGeofenceExited"), object: nil, queue: OperationQueue.main) { (notification) in
+            print("Perform custom action on Geofence Exit event")
+        }
+    }
 }
-
